db7caf772fd8f583d733b627e18e414e2f57dbc8
Sadatsafavi <msafavi@ead.ubc.ca>
2020-01-27 09:07:39 -0800

    First version!

---
+++ R/core.R
@@ -0,0 +1,379 @@
+#2020.01.27
+
+
+library(pROC)
+
+
+
+#Returns the TP/FP data for mROC given a vector of predicted risks
+mROC<-function(p, ordered=F)
+{
+  if(min(p)<0 || max(p)>1) {stop("Error: invalid probability vector."); return(-1); }
+  if(!ordered) p<-p[order(p)]
+
+  sumP1<-sum(p)
+  sumP0<-sum(1-p)
+
+  n<-length(p)
+
+  tp<-0
+  fp<-0
+
+  roc<-cbind(fp=rep(NA,length(p)),tp=rep(NA,length(p)))
+
+  for(i in n:1)
+  {
+    fp<-fp+(1-p[i])/sumP0
+    tp<-tp+p[i]/sumP1
+    roc[i,]<-c(fp,tp)
+  }
+
+  return(roc)
+}
+
+
+
+
+
+calc_mAUC<-function(mROC_data)
+{
+  z<-c(mROC_data[,1],0)
+  w<-z[-length(z)]-z[-1]
+  mAUC=sum(w*mROC_data[,2])/sum(w)
+}
+
+
+
+
+
+#Calculates the asolute surface between the empirical and expected ROCs
+calc_mROC_stats_old<-function(p,y, ordered=F, fast=T)
+{
+  if(!ordered)
+  {
+    o<-order(p)
+    p<-p[o]
+    y<-y[o]
+  }
+
+  if(fast)
+  {
+    tmp<-Ccalc_mROC_stats(p,y)
+    return(c(A=tmp[1],B=tmp[2]))
+  }
+
+  n0<-length(which(y==0))
+  n1<-length(which(y==1))
+
+  n<-n0+n1
+
+  tpO<-0
+  fpO<-0
+
+  sumP1<-sum(p)
+  sumP0<-sum(1-p)
+  tpE<-0
+  fpE<-0
+
+  j<-n
+
+  delta<-0
+
+  for(i in (n0+n1):1)
+  {
+    if(y[i]==1)
+    {
+      tpO<-tpO+1/n1
+    }
+    else
+    {
+      fpO<-fpO+1/n0
+
+      while(fpO>fpE && j>0)
+      {
+        fpE<-fpE+(1-p[j])/sumP0
+        tpE<-tpE+p[j]/sumP1
+        j<-j-1
+        delta<-delta+abs(tpO-tpE)
+        #cat(paste(fpO,",",fpE,";"))
+      }
+      #cat("\n")
+    }
+  }
+  return(delta/n)
+}
+
+
+
+
+
+
+
+
+#Calculates the asolute surface between the empirical and expected ROCs
+calc_mROC_stats<-function(p,y, ordered=F, fast=T)
+{
+  if(!ordered)
+  {
+    o<-order(p)
+    p<-p[o]
+    y<-y[o]
+  }
+  
+  if(fast)
+  {
+    tmp<-Ccalc_mROC_stats(p,y)
+    return(c(A=tmp[1],B=tmp[2]))
+  }
+  
+  n0<-length(which(y==0))
+  n1<-length(which(y==1))
+  n<-n0+n1
+  sumP1<-sum(p)
+  sumP0<-sum(1-p)
+  
+  xo<-0
+  xe<-0
+  yo<-0
+  ye<-0
+  io<-n
+  ie<-n
+  
+  B<-0
+  
+  step<-0
+  
+  #plot(c(0,1),c(0,1))
+  
+  while(io>0 && ie>0)
+  {
+    if(xo<xe) #xo is behind and has to make a jump
+    {
+      if(y[io]==1)
+      {
+        step<-0
+        yo<-yo+1/n1
+      }
+      else
+      {
+        step<-1/n0
+        B<-B+abs(yo-ye)*min(step,xe-xo)
+      }
+      xo<-xo+step
+      io<-io-1
+    }
+    else #now xe is behind
+    {
+      step<-(1-p[ie])/sumP0
+      B<-B+abs(yo-ye)*min(step,xo-xe)
+      xe<-xe+step
+      ye<-ye+p[ie]/sumP1
+      ie<-ie-1
+    }
+    #lines(xe,ye,col='red',type='o')
+    #lines(xo,yo,type='o')
+  }
+  
+  return(list(A=abs(mean(p-y)),B=B))
+}
+
+
+
+
+aux<-environment()
+
+
+
+
+
+#Statistical inference for comparing empirical and expectec ROCs. If CI=TRUE then also returns pointwise CIs
+mROC_inference<-function(p,y,n_sim=100000,CI=FALSE,aux=FALSE,fast=TRUE,conditional=FALSE)
+{
+  out<-list()
+
+  n<-length(p)
+
+  if(aux)
+  {
+    aux$AB<<-matrix(NA, nrow =n_sim, ncol=2)
+  }
+
+  o<-order(p)
+  p<-p[o]
+  y<-y[o]
+  
+  if(conditional)
+  {
+    p1<-t.test(p-y)$p.value
+    
+    n1<-sum(y)
+    a<-p
+    b<-p
+    
+    for(i in 1:n)
+    {
+      a[i]<-p[i]*dpoisbinom(n1-1,p[-i])
+      b[i]<-(1-p[i])*dpoisbinom(n1,p[-i])
+    }
+    
+    pik<-a/(a+b)
+    
+    pikt=UPMEpiktildefrompik(pik)
+  
+    w=pikt/(1-pikt)
+    q=UPMEqfromw(w,n1)
+    
+    if(fast)
+    {
+      tmp=Csimulate_null_ds_conditional_crazy(p,n1,n_sim)
+      #tmp=Csimulate_null_ds_conditional(q,n_sim)
+      stats<-calc_mROC_stats(p,y)
+      out$stats<-stats
+      
+      out$null_stats<-c(distance.se=sqrt(var(tmp[,1]/length(tmp[,1]))),surface.mu=mean(tmp[,2]),surface.se=sqrt(var(tmp[,2]/length(tmp[,2]))))
+      
+      cdf2<-ecdf(x = tmp[,2])
+      
+      p2<-1-cdf2(stats[2])
+      d<- -2*(log(p1)+log(p2))
+      
+      out$stat<-c(value=d,df=4)
+      
+      p3<-1-pchisq(q = d, df = 4)
+      
+      out$pval<-p3
+      out$pvals<-c(distance=p1,surface=p2)
+      
+    }
+    else
+    {
+      #TODO
+    }
+  }
+  else #If conditional
+  {
+    if(fast)
+    {
+      n1<-sum(y)
+      
+      tmp<-Csimulate_null_mROC_stats_unconditional(p,n_sim)
+      
+      stats<-calc_mROC_stats(p,y)
+      out$stats<-stats
+  
+      out$null_stats<-c(A.mu=mean(tmp[,1]),A.se=sqrt(var(tmp[,1]/length(tmp[,1]))),B.mu=mean(tmp[,2]),B.se=sqrt(var(tmp[,2]/length(tmp[,2]))))
+  
+      cdf1<-ecdf(x = tmp[,1])
+      cdf2<-ecdf(x = tmp[,2])
+  
+      p1<-1-cdf1(stats[1])
+      p2<-1-cdf2(stats[2])
+      d<- -2*(log(p1)+log(p2))
+  
+      p1s<-cdf1(tmp[,1])
+      p2s<-cdf2(tmp[,2])
+      ds<--2*(log(p1s)+log(p2s))
+      var_ds<-var(ds)
+      e_ds<-mean(ds)
+      c<-var_ds/2/e_ds
+      k<-2*e_ds^2/var_ds
+  
+      out$stat<-c(value=d/c,df=k)
+  
+      p3<-1-pchisq(q = d/c, df = k)
+  
+      out$pval<-p3
+      out$pvals<-c(A=p1,B=p2)
+  
+      if(aux)
+      {
+        p3s<-1-pchisq(q = ds/c, df = k)
+        aux$ds<<-tmp
+        aux$pvals<<-cbind(p1s,p2s,p3s)
+      }
+    }
+    else #if (fast)
+    {
+      tmp<-matrix(NA,n_sim,2)
+      sns<-matrix(NA,nrow = n+1, ncol = n_sim)
+      for(i in 1:n_sim)
+      {
+        y<-rbinom(n,size = 1,prob=p)
+        if(CI)
+        {
+          res<-roc(y,p,quiet = T)
+          sns[,i]<-coords(res,(0:n)/n,input="specificity",transpose=FALSE)[,"sensitivity",]
+        }
+        tmp[i,]<-calc_mROC_stats(p,y,ordered = TRUE)
+  
+        if(aux)
+        {
+          aux$AB[i,]<<-c(mean(y)-mean(m),tmp[i])
+        }
+      }
+    }
+  } #else conditional
+  if(CI)
+  {
+    tmp2<-apply(sns,MARGIN = 1,FUN = ecdf)
+    out$low<-unlist(lapply(tmp2,quantile,0.025))
+    out$high<-unlist(lapply(tmp2,quantile,0.975))
+  }
+
+
+  return(out)
+}
+
+
+
+
+
+
+
+#Main eRoc analysis: draws the ROC and eROC. inference=0: no inference, inference=1: p-value, inference=2: p-value and 95%CI
+mROC_analysis<-function(p,y,inference=0, n_sim, fast=TRUE)
+{
+  if(inference==2 && fast) stop("Confidence intervals are currently only available when fast=FALSE")
+  
+  roc_data<-pROC::roc(y,p)
+  plot(roc_data)
+
+  out<-list()
+  out$roc_data<-roc_data
+
+  message("AUC is ",roc_data$auc)
+  
+  res<-mROC(p)
+  out$mROC_data<-res
+  
+  message("mAUC is ",calc_mAUC(res))
+
+  lines(1-res[,'fp'],res[,'tp'],col='red')
+
+  if(inference)
+  {
+    inf<-mROC_inference(p=p, y=y, CI=(inference==2), n_sim = n_sim,  fast=fast)
+    if(inference==2)
+    {
+      n<-length(p)
+      lines((0:n)/n,inf$low,type='l',col="gray")
+      lines((0:n)/n,inf$high,type='l',col="gray")
+    }
+    out$inference<-inf
+    message("Test statistic is ",inf$stat,"\n")
+    message("p-value is ",inf$pval,"\n")
+  }
+
+  return(out)
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/fast.cpp
@@ -0,0 +1,455 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+
+
+enum errors
+{
+  ERR_INCORRECT_SETTING_VARIABLE=-1,
+  ERR_INCORRECT_VECTOR_SIZE=-2,
+  ERR_INCORRECT_INPUT_VAR=-3,
+  ERR_EVENT_STACK_FULL=-4,
+  ERR_MEMORY_ALLOCATION_FAILED=-5
+} errors;
+/*** R
+errors<-c(
+  ERR_INCORRECT_SETTING_VARIABLE=-1,
+  ERR_INCORRECT_VECTOR_SIZE=-2,
+  ERR_INCORRECT_INPUT_VAR=-3,
+  ERR_EVENT_STACK_FULL=-4,
+  ERR_MEMORY_ALLOCATION_FAILED=-5
+)
+*/
+
+
+
+
+#define AS_VECTOR_DOUBLE(src) std::vector<double>(&src[0],&src[0]+sizeof(src)/sizeof(double))
+#define AS_VECTOR_DOUBLE_SIZE(src,size) std::vector<double>(&src[0],&src[0]+size)
+
+#define AS_VECTOR_BOOL_SIZE(src,size) std::vector<bool>(&src[0],&src[0]+size)
+
+#define AS_MATRIX_DOUBLE(src)  array_to_Rmatrix(std::vector<double>(&src[0][0],&src[0][0]+sizeof(src)/sizeof(double)),sizeof(src[0])/sizeof(double))
+#define AS_MATRIX_DOUBLE_SIZE(src,size)  array_to_Rmatrix(std::vector<double>(&src[0][0],&src[0][0]+size*sizeof(src[0])/sizeof(double)),sizeof(src[0])/sizeof(double))
+
+#define AS_MATRIX_INT(src)  array_to_Rmatrix(std::vector<int>(&src[0][0],&src[0][0]+sizeof(src)/sizeof(int)),sizeof(src[0])/sizeof(int))
+#define AS_MATRIX_INT_SIZE(src,size)  array_to_Rmatrix(std::vector<int>(&src[0][0],&src[0][0]+size*sizeof(src[0])/sizeof(int)),sizeof(src[0])/sizeof(int))
+
+#define AS_VECTOR_INT(src) std::vector<int>(&src[0],&src[0]+sizeof(src)/sizeof(int))
+#define AS_VECTOR_INT_SIZE(src,size) std::vector<int>(&src[0],&src[0]+size)
+
+#define READ_R_VECTOR(src,dest) std::copy(src.begin(),src.end(),&dest[0])
+#define READ_R_MATRIX(src,dest) {if(src.size()==sizeof(dest)/sizeof(dest[0][0])) {std::copy(src.begin(),src.end(),&dest[0][0]);} else return(ERR_INCORRECT_VECTOR_SIZE);}
+
+
+
+
+
+
+
+double rand_unif()
+{
+  return((double)rand()/RAND_MAX);
+}
+
+
+typedef struct mROC_stats
+{
+  double A;
+  double B;
+} mROC_stats;
+
+
+int int_buffer[1000000];
+double double_buffer[1000000];
+bool bool_buffer[1000000];
+mROC_stats mROC_stats_buffer[1000000];
+
+
+//Calculates the A (distance) and B (ROC equality) statistics for a vector of predicted probabilities (PI) and observed outcomes (Y)//
+mROC_stats calc_mROC_stats(int n, double *M, int *Y)
+{
+  mROC_stats out;
+  out.A=0;
+  out.B=0;
+
+  int n0=0,n1=0;
+  double sumP0=0,sumP1=0;
+
+  for(int i=0;i<n;i++)
+  {
+    if(Y[i]==0) ++n0; else ++n1;
+    sumP1+=M[i];
+    sumP0+=1-M[i];
+    out.A+=Y[i]-M[i];
+  }
+  out.A=fabs(out.A/n);
+
+
+  double xo=0, xe=0;
+  double yo=0,ye=0;
+
+  double step=0;
+  double B=0;
+
+  int io=n-1,ie=n-1;
+  
+  while(io>=0 && ie>=0)
+  {
+    if(xo<xe) //xo is behind and has to make a jump
+    {
+      if(Y[io]==1)
+      {
+        step=0;
+        yo=yo+(double)1/n1;
+      }
+      else
+      {
+        step=(double)1/n0;
+        B=B+fabs(yo-ye)*std::min(step,xe-xo);
+      }
+      xo=xo+step;
+      io=io-1;
+    }
+    else //now xe is behind
+    {
+      step=(1-M[ie])/sumP0;
+      B=B+fabs(yo-ye)*std::min(step,xo-xe);
+      xe=xe+step;
+      ye=ye+M[ie]/sumP1;
+      ie=ie-1;
+    }
+  }
+  
+  out.B=B;
+  return(out);
+}
+
+
+
+
+
+
+// [[Rcpp::export]]
+std::vector<double> Ccalc_mROC_stats(NumericVector M, NumericVector Y)
+{
+  int n=M.size();
+  double m[n];
+  int y[n];
+  READ_R_VECTOR(M,m);
+  READ_R_VECTOR(Y,y);
+  mROC_stats out=calc_mROC_stats(n,m,y);
+  double tmp[2];
+  tmp[0]=out.A;
+  tmp[1]=out.B;
+  return(AS_VECTOR_DOUBLE_SIZE(tmp,2));
+}
+
+
+
+
+
+
+
+
+
+
+//Conditionaly samples from vector pof probabilities p, such that m individuals are selected.
+int *conditional_sample(int n, double *p, int size)
+{
+  double odds[n];
+  double sum_odds=0;
+
+  for(int i=0;i<n;i++)
+  {
+    odds[i]=p[i]/(1-p[i]);
+    sum_odds=sum_odds+odds[i];
+    int_buffer[i]=0;
+  }
+
+  for(int i=0;i<size;i++)
+  {
+    double r=runif(1)[0]*sum_odds;
+    int j=-1;
+    double rsum=0;
+    while(rsum<r)
+    {
+      ++j;
+      if(int_buffer[j]==0)  rsum=rsum+odds[j];
+    }
+    int_buffer[j]=1;
+    sum_odds=sum_odds-odds[j];
+  }
+
+  return(int_buffer);
+}
+
+
+
+
+
+
+//CRAZY: conditionally samples through try and error!!!
+int *conditional_sample_crazy(int n, double *p, int size)
+{
+  int current_size=-1;
+  int *y=int_buffer;
+
+  while(current_size!=size)
+  {
+    current_size=0;
+    for(int j=0;j<n;j++)
+    {
+      if(rand_unif()<p[j]) {y[j]=1; ++current_size;} else y[j]=0;
+    }
+  }
+
+  return(y);
+}
+
+
+
+
+
+
+// [[Rcpp::export]]
+double test()
+{
+  return(runif(1)[0]);
+}
+
+
+
+
+
+
+
+
+
+
+mROC_stats *simulate_null_mROC_stats_unconditional(int n, double *M, int n_sim)
+{
+  int y[n];
+  for(int i=0;i<n_sim;i++)
+  {
+    for(int j=0;j<n;j++)
+    {
+      if(rand_unif()<M[j]) y[j]=1; else y[j]=0;
+    }
+    mROC_stats_buffer[i]=calc_mROC_stats(n,M,y);
+  }
+  return(mROC_stats_buffer);
+}
+
+
+
+
+
+// [[Rcpp::export]]
+NumericMatrix Csimulate_null_mROC_stats_unconditional(NumericVector M, int n_sim)
+{
+  int n=M.size();
+  double m[n];
+  std::copy(M.begin(),M.end(),m);
+
+  mROC_stats *tmp=simulate_null_mROC_stats_unconditional(n,m,n_sim);
+
+  NumericMatrix out(n_sim,2);
+  for(int i=0;i<n_sim;i++)
+  {
+    out(i,0)=tmp[i].A;
+    out(i,1)=tmp[i].B;
+  }
+  return(out);
+}
+
+
+
+
+
+
+
+/*
+
+
+int *UPMEsfromq(double *q, int n_row, int n_col)
+{
+  //printf("UPMEsfromq: You want me to sample %d from a vector of size %d\n",n_col,n_row);
+  int *out=int_buffer;
+  
+  int col_index=n_col-1;
+  
+  for (int k=0;k<n_row;k++)
+  {
+    out[k]=0;
+    //printf("%d,%d,%f\n",k,col_index,q[col_index*(n_row)+k]);
+    if (col_index != -1)
+    {
+      if (rand_unif() < q[col_index*(n_row)+k]) 
+      {
+        out[k] = 1;
+        col_index = col_index - 1;
+      }
+    }
+  }
+  
+  int sum=0;
+  for(int i=0;i<n_row;i++)  sum=sum+out[i];
+  
+  //printf("Final: %d,%d\n",col_index,sum);
+  
+  return(out);
+}
+
+
+
+
+
+// [[Rcpp::export]]
+NumericVector CUPMEsfromq(NumericMatrix q)
+//For test purposes.
+{
+  int n_row=q.nrow();
+  int n_col=q.ncol();
+  
+  NumericVector outV(n_row);
+  
+  for (int k=0;k<n_row;k++)
+  {
+    outV(k)=0;
+    if (n_col != -1)
+    {
+      //printf("%d,%d,%f\n",k,n_col,q(n_row,n_col-1));
+      if (rand_unif() < q(k,n_col-1))
+      {
+        outV(k) = 1;
+        n_col = n_col - 1;
+      }
+    }
+  }
+  
+    return(outV);
+}
+
+
+
+
+
+mROC_stats *simulate_null_ds_conditional(double *q, int n_row, int n_col, int n_sim)
+{
+  //printf("simulate_null_ds_conditional: You want me to sample %d from a vector of size %d\n",n_col,n_row);
+  int *y;
+  for(int i=0;i<n_sim;i++)
+  {
+    y=UPMEsfromq(q, n_row, n_col);
+    mROC_stats_buffer[i]=calc_mROC_stats(n_row,q,y);
+  }
+  return(mROC_stats_buffer);
+}
+
+
+
+
+// [[Rcpp::export]]
+NumericMatrix Csimulate_null_ds_conditional(NumericMatrix q, int n_sim)
+{
+//Number of rows in q is the size of vector; number of oclumns is desdired sample size
+  int n_row=q.nrow();
+  int n_col=q.ncol();
+  
+  //printf("Csimulate_null_ds_conditional: You want me to sample %d from a vector of size %d\n",n_col,n_row);
+  
+  double _q[n_row*n_col];
+  std::copy(q.begin(),q.end(),_q);
+  
+  //return(NumericMatrix(n_row,n_col,_q));
+  
+  mROC_stats *tmp=simulate_null_ds_conditional(_q, n_row, n_col, n_sim);
+  
+  NumericMatrix out(n_sim,2);
+  for(int i=0;i<n_sim;i++)
+  {
+    
+    out(i,0)=tmp[i].A;
+    out(i,1)=tmp[i].B;
+  }
+  return(out);
+}
+
+
+
+
+
+
+mROC_stats *simulate_null_ds_conditional_crazy(double *p, int vlength, int size, int n_sim)
+{
+  //printf("simulate_null_ds_conditional: You want me to sample %d from a vector of size %d\n",n_col,n_row);
+  int *y;
+  for(int i=0;i<n_sim;i++)
+  {
+    y=conditional_sample_crazy(vlength, p, size);
+    mROC_stats_buffer[i]=calc_mROC_stats(vlength,p,y);
+  }
+  return(mROC_stats_buffer);
+}
+
+
+
+
+// [[Rcpp::export]]
+NumericMatrix Csimulate_null_ds_conditional_crazy(NumericVector p, int size, int n_sim)
+{
+  double _p[p.length()];
+  
+  std::copy(p.begin(),p.end(),_p);
+  
+  mROC_stats *tmp=simulate_null_ds_conditional_crazy(_p, p.length(), size, n_sim);
+  
+  NumericMatrix out(n_sim,2);
+  for(int i=0;i<n_sim;i++)
+  {
+    
+    out(i,0)=tmp[i].A;
+    out(i,1)=tmp[i].B;
+  }
+  return(out);
+}
+
+
+
+
+
+
+// [[Rcpp::export]]
+std::vector<int> Cconditional_sample(NumericMatrix q)
+{
+  int n_row=q.nrow();
+  int n_col=q.ncol();
+  
+  double _q[n_row*n_col];
+  std::copy(q.begin(),q.end(),_q);
+  
+  int *out=int_buffer;
+  out=UPMEsfromq(_q, n_row, n_col);
+  
+  return(AS_VECTOR_INT_SIZE(out,n_row));
+}
+
+
+
+
+// [[Rcpp::export]]
+std::vector<int> Cconditional_sample_crazy(NumericVector M, int size)
+{
+  int n=M.size();
+  double m[n];
+  std::copy(M.begin(),M.end(),m);
+  int *tmp=conditional_sample_crazy(n,m,size);
+  
+  return(AS_VECTOR_INT_SIZE(tmp,n));
+}
+
+
+*/
+
