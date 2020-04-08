2d1e0b510ef9488e6f85c693e3d032baf572a470
Sadatsafavi <msafavi@ead.ubc.ca>
2020-04-08 12:29:59 -0700

    Minor tweaks before manuscript submission

--- paperCode/mROC.paperCode.R
+++ paperCode/mROC.paperCode.R
@@ -106,7 +106,7 @@ calibration_plot<-function(p,y,n_bins=10,type_prob=TRUE)
 
 
 
-############Section 1 code: stylized example######################
+############Section 1: stylized example######################
 
 
 stylized_example<-function()
@@ -514,176 +514,6 @@ detailed_sim_power<-function(sample_sizes=c(100,250,1000), X_dist=c(0,1), b0s=c(
 
 
 
-detailed_sim_quadratic<-function(sample_sizes=c(100,250,1000), X_dist=c(0,1), b0s=c(-1,-0.5,0,0.5,1),b1s=c(-1.41,0.7,0,0.7,1.41),n_sim=1000, draw_plots="", GRuse=FALSE)
-{
-  b1<-1 
-  pi<-runif(100)
-  y<-rbinom(100,1,pi)
-  template<-unlist(mROC_inference(y=y,p=pi,CI=FALSE, n_sim = 100,fast=TRUE))
-  
-  out<-as.data.frame(matrix(NA, nrow =n_sim*length(sample_sizes)*length(b0s)*length(b1s),ncol = 4+length(template)+1))
-  colnames(out)<-c("i_sim","sample_size", "b0", "b1", names(template),"pval.LRT")
-  
-  index<-1
-  for(i in 1:n_sim)
-  {
-    cat(".")
-    for(j in 1:length(sample_sizes))
-    {
-      ss<-sample_sizes[j]
-      if(is.null(X_dist)) pi<-runif(ss) else  pi<-1/(1+exp(-rnorm(ss,X_dist[1],X_dist[2])))
-      for(k0 in 1:length(b0s))  
-      {
-        b0<-b0s[k0]
-        for(k1 in 1:length(b1s))
-        {
-          #message(paste(ss,b0,b1,sep=","))
-          b2<-b1s[k1]
-          logit.pi<-log(pi/(1-pi))
-          logit.pi_star<-b0+b1*logit.pi+b2*logit.pi*logit.pi
-          pi_star<-1/(1+exp(-logit.pi_star))
-          
-          repeat
-          {
-            y=rbinom(ss,size = 1,prob = pi)
-            if(sum(y)>=3 && sum(1-y)>=3) 
-              break 
-            else
-            {
-              message("OUCH - bad sample, too few outcomes, repeat!")
-            }
-          }
-          
-          if(draw_plots!="")
-            if(i==1 && j==length(sample_sizes))
-            {
-              #par(mar=c(1,1,1,1))
-              #par(mfrow=c(length(b0s),length(b1s)))
-              
-              aux$y <<- y
-              aux$pi_star <<- pi_star
-              aux$pi <<- pi
-              
-              if(draw_plots=="pp")
-              {
-                q<-(0:100)/100
-                q_x<-log(q/(1-q))
-                
-                if(b2==0)
-                {
-                  root<-(q_x-b0)/b1
-                  q_p<-1/(1+exp(-root))
-                }
-                else
-                {
-                  delta<- b1^2-4*b2*(b0-q_x)
-                  good<-which(delta>0)
-                  q_p<-q
-                  q_p[-good]<-NA
-                  root1 <- (-b1-sqrt(delta[good]))/2/b2
-                  root2 <- (-b1+sqrt(delta[good]))/2/b2
-                  q_p[good] <- (dnorm(root1)*1/(1+exp(-root1))+dnorm(root2)*1/(1+exp(-root2)))/(dnorm(root1)+dnorm(root2))
-                }
-                
-                plot(q,q_p,xlab=(paste(ss,b0,b1,sep=",")), ann=FALSE, xaxt='n', yaxt='n', type='l', xlim=c(0,1), ylim=c(0,1), col="blue", lwd=2)
-                lines(c(0,1),c(0,1),col="gray",type='l')
-                text(0.50,0.075, sprintf("E(\U03C0*)=%s",format(mean(pi_star),digits =  3)))
-                #text(0.75,0.5, sprintf("E(pi)=%s",format(mean(pi),digits = 3)))
-                title(sprintf(paste0("\U03B2","0=%s,\U03B2","1=%s"),format(b0,3),format(b2,3)))
-              }
-              if(draw_plots=="roc")
-              {
-                plot(c(0,1),c(0,1),col="grey")
-                for(i in 1:100)
-                {
-                  yy<-rbinom(length(pi_star),1,pi_star)
-                  tmp<-roc(yy,pi_star)
-                  lines(1-tmp$specificities,tmp$sensitivities,col="grey")
-                }
-                tmp<-pROC::roc(y,pi_star)
-                lines(1-tmp$specificities,tmp$sensitivities, type='l', ann=FALSE, xaxt='n', yaxt='n')
-                AUC=tmp$auc*1
-                tmp<-mROC(pi_star)
-                mAUC=mAUC(tmp)
-                B=calc_mROC_stats(y,pi_star)['B']
-                #text(0.5,0.5, sprintf("AUC:%s",format(AUC, digits = 2)),cex = 1) AUC is constant! 
-                text(0.5,0.3, sprintf("mAUC:%s",format(mAUC, digits = 2)),cex=1)
-                text(0.5,0.1, sprintf("B:%s",format(B, digits = 2)),cex=1)
-                sf<-stepfun(tmp$FPs,c(0,tmp$TPs))
-                lines(sf,col="red")
-                #title(sprintf("b0=%s,b1=%s",format(b0,3),format(b1,3)))
-              }
-              if(draw_plots=="logit")
-              {
-                plot(logit.pi_star,logit.pi,xlim=c(-3,3),ylim=c(-3,3))
-                lines(c(-3,3),c(-3,3))
-                text(0.50,0.075, sprintf("E(\U03C0*)=%s",format(mean(pi_star),digits =  3)))
-                #text(0.75,0.5, sprintf("E(pi)=%s",format(mean(pi),digits = 3)))
-                title(sprintf(paste0("\U03B2","0=%s,\U03B2","1=%s"),format(b0,3),format(b1,3)))
-              }
-              if(draw_plots=="logitE")
-              {
-                q_x<-(-300:300)/100
-                if(b2==0)
-                {
-                  root<-(q_x-b0)/b1
-                  q_y<-root
-                }
-                else
-                {
-                  delta<- b1^2-4*b2*(b0-q_x)
-                  good<-which(delta>0)
-                  q_y<-q_x
-                  q_y[-good]<-NA
-                  root1 <- (-b1-sqrt(delta[good]))/2/b2
-                  root2 <- (-b1+sqrt(delta[good]))/2/b2
-                  q_y[good] <- (dnorm(root1)*root1+dnorm(root2)*root2)/(dnorm(root1)+dnorm(root2))
-                }
-                
-                plot(q_x,q_y,xlab=(paste(ss,b0,b1,sep=",")), ann=FALSE, xaxt='n', yaxt='n', type='l', xlim=c(-3,3), ylim=c(-3,3), col="green", lwd=2)
-                lines(c(-3,3),c(-3,3),col="gray",type='l')
-                text(0.50,0.075, sprintf("E(\U03C0*)=%s",format(mean(pi_star),digits =  3)))
-                #text(0.75,0.5, sprintf("E(pi)=%s",format(mean(pi),digits = 3)))
-                title(sprintf(paste0("\U03B2","0=%s,\U03B2","1=%s"),format(b0,3),format(b2,3)))
-              }
-            }
-          
-          #message(paste(ss,b0,b1,sep=","))
-          
-          tmp<-unlist(mROC_inference(y=y,p=pi_star,CI=FALSE, n_sim = settings$n_sim_inference_fast, fast=TRUE))
-          out[index,1]<-i
-          out[index,2]<-ss
-          out[index,3]<-b0
-          out[index,4]<-b1
-          out[index,5:(5+length(template)-1)]<-tmp
-          logit.pi_star<-log(pi_star/(1-pi_star))
-          f.0<-glm(y~ -1+offset(logit.pi_star),family="binomial")
-          #f.a<-glm(y~offset(logit.pi_star),family="binomial")
-          f.ab<-glm(y~logit.pi_star,family="binomial")
-          message(coefficients(f.ab))
-          p.val.ab<-1-pchisq(f.0$deviance-f.ab$deviance,2)
-          out[index,"pval.LRT"]<-p.val.ab
-          index<-index+1
-        }
-      }
-    }
-    
-    if((i%%10)==0 && GRuse)
-    {
-      GRpush(out,overWrite = T)
-      cat("Pushed at i=",i," \n")
-    }
-  }
-  
-  aux$out<<-out
-  
-  return(out)
-}
-
-
-
-
-
 
 
 
@@ -796,12 +626,12 @@ internal_formatter<-function(data, n_col=4)
 
 
 
-##########################################Section 5: new case study#####################################################
+##########################################Section 4: case study#####################################################
 
 library("haven")
 
 
-new_case_study<-function(covars=c("gender","age10","oxygen","hosp1yr","sgrq10","fev1","nowsmk","LABA","LAMA"),only_severe=FALSE, do_recalibrate=FALSE)
+case_study<-function(covars=c("gender","age10","oxygen","hosp1yr","sgrq10","fev1","nowsmk","LABA","LAMA"),only_severe=FALSE, do_recalibrate=FALSE)
 {
   results<-list()
 
