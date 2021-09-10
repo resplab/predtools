c181ffeec8bc968dcf64724d0d869429a1b38c77
Amin Adibi <adibi@alumni.ubc.ca>
2021-09-10 10:35:13 -0700

    dependencies updated

--- R/predtools.R
+++ R/predtools.R
@@ -1,5 +1,7 @@
 #' predtools: A package for various methods in predictive analytics.
 #' @import pROC
+#' @importFrom graphics lines par
+#' @importFrom stats ecdf pchisq quantile rbinom stepfun t.test
 #' @docType package
 #' @name predtools
 #' @useDynLib predtools
