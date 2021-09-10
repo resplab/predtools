f76764213dd5cad11073b101485baa66a1849337
Amin Adibi <adibi@alumni.ubc.ca>
2021-09-10 16:45:42 -0700

    clean up c

--- R/predtools.R
+++ R/predtools.R
@@ -1,8 +1,9 @@
 #' predtools: A package for various methods in predictive analytics.
+#' @importFrom Rcpp sourceCpp
 #' @import pROC
 #' @importFrom graphics lines par
 #' @importFrom stats ecdf pchisq quantile rbinom stepfun t.test
 #' @docType package
 #' @name predtools
-#' @useDynLib predtools
+#' @useDynLib predtools, .registration = TRUE
 NULL
