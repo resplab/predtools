37e5c338d1479ba76c1ca39a06fd2c8a71422c90
Amin Adibi <adibi@alumni.ubc.ca>
2021-09-14 12:46:03 -0700

    data cleaned up and documented

--- R/val_data.R
+++ R/val_data.R
@@ -5,10 +5,10 @@
 #' @format A data frame with 400 rows and 5 variables:
 #' \itemize{
 #'   \item{age}{age of the patient}
-#'   \item{severity}{dooshvari}
-#'   \item{sex}{dooshvari}
-#'   \item{comorbidity}{dooshvari}
-#'   \item{y}{dooshvari}
+#'   \item{severity}{whether or not the disease was severe}
+#'   \item{sex}{binary sex variable, 1 for female and 0 for male}
+#'   \item{comorbidity}{whether or not comorbidities are present}
+#'   \item{y}{response variable}
 #' }
 #' @docType data
 #' @keywords datasets
