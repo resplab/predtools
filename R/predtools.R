361bfab76f71ae5ae28dff82af961674e2562f90
Amin Adibi <adibi@alumni.ubc.ca>
2021-09-16 13:46:47 -0700

    dependency now added

--- R/predtools.R
+++ R/predtools.R
@@ -1,6 +1,7 @@
 #' @importFrom Rcpp sourceCpp
 #' @import pROC
 #' @importFrom graphics lines par
+#' @importFrom RConics cubic
 #' @importFrom stats ecdf pchisq quantile rbinom stepfun t.test
 #' @useDynLib predtools, .registration = TRUE
 NULL
