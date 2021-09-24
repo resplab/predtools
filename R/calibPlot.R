8094269f4957ed49c5e2d6398071a576903c0dab
Abdollah <a.saffari72@gmail.com>
2021-09-24 15:05:58 -0700

    Update calibPlot function and add a vignette for the same function

--- R/calibPlot.R
+++ R/calibPlot.R
@@ -3,15 +3,13 @@
 #' @param data Data include observed and predicted outcomes.
 #' @param obs Name of observed outcome in the input data.
 #' @param follow_up Name of follow-up time (if applicable) in the input data.
-#' @param pred_1 Name of first predicted outcome in the input data.
-#' @param pred_2 Name of second (if applicable) predicted outcome in the input data.
+#' @param pred Name of first predicted outcome in the input data.
+#' @param group Name of grouping column (if  applicable) in the input data.
 #' @param nTiles Number of tiles (e.g., 10 for deciles) in the calibration plot.
 #' @param legendPosition Legend position on the calibration plot.
 #' @param title Title on the calibration plot.
 #' @param x_lim Limits of x-axis on the calibration plot.
 #' @param y_lim Limits of y-axis on the calibration plot.
-#' @param model_label_1 Label of the first prediction set.
-#' @param model_label_2 Label of the first prediction set.
 #' @param xlab Label of x-axis on the calibration plot.
 #' @param ylab Label of y-axis on the calibration plot.
 #' @param points_col_list Points' color on the calibration plot.
@@ -22,8 +20,8 @@
 #' library(dplyr)
 #' x <- rnorm(100, 10, 2)
 #' y <- x + rnorm(100,0, 1)
-#' data <- tibble(x,y)
-#' calibration_plot(data, obs = "x", pred_1 = "y")
+#' data <- data.frame(x, y)
+#' calibration_plot(data, obs = "x", pred = "y")
 #' @return Returns calibration plot (a ggplot object) and a dataset including summary statistics of
 #' the predicted and observed outcomes (if data_summary set to be TRUE).
 #' @export
@@ -31,43 +29,35 @@
 calibration_plot <- function(data,
                              obs,
                              follow_up = NULL,
-                             pred_1,
-                             pred_2 = NULL,
+                             pred,
+                             group = NULL,
                              nTiles = 10,
                              legendPosition = "right",
                              title = NULL,
                              x_lim = NULL,
                              y_lim = NULL,
-                             model_label_1 = NULL,
-                             model_label_2 = NULL,
                              xlab = "Prediction",
                              ylab = "Observation",
                              points_col_list = NULL,
                              data_summary = FALSE) {
   
-  if (! exists("obs") | ! exists("pred_1")) stop("obs and pred_1 can not be null.")
+  if (! exists("obs") | ! exists("pred")) stop("obs and pred can not be null.")
+  
+  n_groups <- length(unique(data[ , group]))
   
   data %<>%
-    mutate(decile = ntile(!!sym(pred_1), nTiles))
+    mutate(decile = ntile(!!sym(pred), nTiles))
   
   if (is.null(follow_up)) data$follow_up <- 1
 
-  if (! is.null(pred_2)) {
+  if (! is.null(group)) {
     data %>%
-      group_by(.data$decile) %>%
+      group_by(.data$decile, !!sym(group)) %>%
       summarise(obsRate = mean(!!sym(obs) / follow_up, na.rm = T),
                 obsRate_SE = sd(!!sym(obs) / follow_up, na.rm = T) / sqrt(n()),
                 obsNo = n(),
-                model = ifelse(is.null(model_label_1), "Model 1", model_label_1),
-                predRate = mean(!!sym(pred_1), na.rm = T)) -> dataDec_mod_1
-    data %>%
-      group_by(.data$decile) %>%
-      summarise(obsRate = mean(!!sym(obs) / follow_up, na.rm = T),
-                obsRate_SE = sd(!!sym(obs) / follow_up, na.rm = T) / sqrt(n()),
-                obsNo = n(),
-                model = ifelse(is.null(model_label_1), "Model 2", model_label_2),
-                predRate = mean(!!sym(pred_2), na.rm = T)) -> dataDec_mod_2
-    dataDec_mods <- rbind(dataDec_mod_1, dataDec_mod_2)
+                predRate = mean(!!sym(pred), na.rm = T)) -> dataDec_mods
+    colnames(dataDec_mods)[colnames(dataDec_mods) == "group"] <- group
   }
   else {
     data %>%
@@ -75,28 +65,27 @@ calibration_plot <- function(data,
       summarise(obsRate = mean(!!sym(obs) / follow_up, na.rm = T),
                 obsRate_SE = sd(!!sym(obs) / follow_up, na.rm = T) / sqrt(n()),
                 obsNo = n(),
-                model = model_label_1,
-                predRate = mean(!!sym(pred_1), na.rm = T)) -> dataDec_mods
+                predRate = mean(!!sym(pred), na.rm = T)) -> dataDec_mods
   }
 
   dataDec_mods$obsRate_UCL <- dataDec_mods$obsRate + 1.96 * dataDec_mods$obsRate_SE
   dataDec_mods$obsRate_LCL <- dataDec_mods$obsRate - 1.96 * dataDec_mods$obsRate_SE
   
+  dataDec_mods <- as.data.frame(dataDec_mods)
   
-  if (! is.null(pred_2)) {
+  if (! is.null(group)) {
+    dataDec_mods[ , group] <- factor(dataDec_mods[ , group])
     calibPlot_obj <-
-      ggplot(data = dataDec_mods, aes(y = .data$obsRate, x = .data$predRate, group = .data$model, color = .data$model)) +
+      ggplot(data = dataDec_mods, aes(y = .data$obsRate, x = .data$predRate, group = !!sym(group), color = !!sym(group))) +
       geom_point() +
-      lims(x = ifelse(c(is.null(x_lim), is.null(x_lim)), c(min(dataDec_mods$predRate), max(dataDec_mods$predRate)), x_lim),
-           y = ifelse(c(is.null(y_lim), is.null(y_lim)),
-                      c(min(dataDec_mods$obsRate_LCL), max(dataDec_mods$obsRate_UCL)), y_lim)) +
+      lims(x = ifelse(rep(is.null(x_lim), 2), c(min(dataDec_mods$predRate), max(dataDec_mods$predRate)), x_lim),
+           y = ifelse(rep(is.null(y_lim), 2), c(min(dataDec_mods$obsRate_LCL), max(dataDec_mods$obsRate_UCL)), y_lim)) +
       geom_errorbar(aes(ymax = .data$obsRate_UCL, ymin = .data$obsRate_LCL)) +
       geom_abline(intercept = 0, slope = 1) +
-      scale_color_manual(values = ifelse(c(is.null(points_col_list), is.null(points_col_list)),
-                                         c(ggplot2::scale_colour_brewer(palette = "Set3")$palette(8)[4],
-                                           ggplot2::scale_colour_brewer(palette = "Set3")$palette(8)[5]),
+      scale_color_manual(values = ifelse(rep(is.null(points_col_list), n_groups),
+                                         (ggplot2::scale_colour_brewer(palette = "Set3")$palette(8)[c(4 : 8, 1 : 3)])[c(1 : n_groups)],
                                          points_col_list)) +
-      labs(x = ifelse(is.null(xlab), pred_1, xlab),
+      labs(x = ifelse(is.null(xlab), pred, xlab),
            y = ifelse(is.null(ylab), obs, ylab),
            title = title) +
       theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
@@ -109,9 +98,8 @@ calibration_plot <- function(data,
     calibPlot_obj <-
       ggplot(data = dataDec_mods, aes(y = .data$obsRate, x = .data$predRate)) +
       geom_point(color = ggplot2::scale_colour_brewer(palette = "Set3")$palette(8)[5]) +
-      lims(x = ifelse(c(is.null(x_lim), is.null(x_lim)), c(min(dataDec_mods$predRate), max(dataDec_mods$predRate)), x_lim),
-           y = ifelse(c(is.null(y_lim),  is.null(y_lim)),
-                      c(min(dataDec_mods$obsRate_LCL), max(dataDec_mods$obsRate_UCL)), y_lim)) +
+      lims(x = ifelse(rep(is.null(x_lim), 2), c(min(dataDec_mods$predRate), max(dataDec_mods$predRate)), x_lim),
+           y = ifelse(rep(is.null(y_lim), 2), c(min(dataDec_mods$obsRate_LCL), max(dataDec_mods$obsRate_UCL)), y_lim)) +
       geom_errorbar(aes(ymax = .data$obsRate_UCL, ymin = .data$obsRate_LCL),
                     col = ifelse(is.null(points_col_list),
                                  ggplot2::scale_colour_brewer(palette = "Set3")$palette(8)[5],
@@ -120,7 +108,7 @@ calibration_plot <- function(data,
       scale_color_manual(values = ifelse(is.null(points_col_list),
                                          ggplot2::scale_colour_brewer(palette = "Set3")$palette(8)[5],
                                          points_col_list)) +
-      labs(x = ifelse(is.null(xlab), pred_1, xlab),
+      labs(x = ifelse(is.null(xlab), pred, xlab),
            y = ifelse(is.null(ylab), obs, ylab),
            title = title) +
       theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
