b1ad2ce59c4ac5898267289a6335e07ac64ba11c
amin <adibi@alumni.ubc.ca>
2022-07-08 14:06:19 -0700

    build errors fixed

--- R/predtools.R
+++ R/predtools.R
@@ -2,10 +2,11 @@
 #' @import pROC
 #' @import ggplot2
 #' @import dplyr
-#' @import mvtnorm
+#' @importFrom mvtnorm pmvnorm
 #' @import magrittr
 #' @importFrom graphics lines par
 #' @importFrom RConics cubic
+#' @importFrom stats dnorm pnorm
 #' @importFrom stats ecdf pchisq quantile rbinom stepfun t.test sd
 #' @useDynLib predtools, .registration = TRUE
 NULL
